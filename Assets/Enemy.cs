144c41b68ccd5c110a4540a3a11d9403c32f2603
Tommy Nguyen <tommyhnguyen03@gmail.com>
2022-02-07 18:16:11 -0800

    enemy health

---
+++ Assets/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField]
+    private int health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+
+    
+
+    void FixedUpdate()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
