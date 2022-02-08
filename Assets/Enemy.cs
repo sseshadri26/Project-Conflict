c674cadbcd33a12f65c31007ddb4aa524cdeefd1
Kaiway <85521741+Kaiway@users.noreply.github.com>
2022-02-07 18:47:32 -0800

    placeholder enemy sprite

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,29 +7,55 @@ public class Enemy : MonoBehaviour
 
     [SerializeField]
     private int health;
+    private float speed;
+
+    int abilityTmr;
+
+    public Transform playerOneTrfm;
+    public Transform playerTwoTrfm;
+
+    Transform trfm;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        trfm = transform;
     }
 
 
 
 
-    
 
     void FixedUpdate()
     {
-
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
         
+        if (Vector2.Distance(playerOneTrfm.position, trfm.position) < Vector2.Distance(playerTwoTrfm.position, trfm.position)) //if player one is closer to this enemy
+        {
+            //target player one   
+            trfm.rotation = Quaternion.AngleAxis(Mathf.Atan2(trfm.position.y - playerOneTrfm.position.y, trfm.position.x - playerOneTrfm.position.x) * Mathf.Rad2Deg + 90, Vector3.forward);
+        } else
+        {
+            //target player two
+            trfm.rotation = Quaternion.AngleAxis(Mathf.Atan2(trfm.position.y - playerTwoTrfm.position.y, trfm.position.x - playerTwoTrfm.position.x) * Mathf.Rad2Deg + 90, Vector3.forward);
+        }
+        trfm.position += trfm.up * speed;
+        //trfm.position += trfm.right * .05f;
+
+
+
+        if (abilityTmr > 0)
+        {
+            abilityTmr--;
+        } else
+        {
+            trfm.position += trfm.up * 5; //dont run 50x per sec
+
+            abilityTmr = 100;
+        }
     }
+
+
+    
     
 }
