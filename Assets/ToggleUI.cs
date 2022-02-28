acf63513d6f9bd44c9f15fd7f35bc384bc00216f
ryankkien <93295033+ryankkien@users.noreply.github.com>
2022-02-28 14:02:07 -0800

    Update ToggleUI.cs

--- Assets/ToggleUI.cs
+++ Assets/ToggleUI.cs
@@ -8,13 +8,16 @@ public class ToggleUI : MonoBehaviour
     CanvasGroup canvasGroup;
     bool i;
 
-    void Start()
-    {
+    void UIToggle(bool i, object CanvasGroup) {
         canvasGroup = GetComponent<CanvasGroup>();
         if (i == true)
         {
             canvasGroup.alpha = 1;
         }
         else canvasGroup.alpha = 0;
+    }
+void Start()
+    {
+
     }
 }
