8a2007fc2d4adf93d685989f8a50329de21a3bac
Kaiway <85521741+Kaiway@users.noreply.github.com>
2022-03-23 11:25:42 -0700

    weapon stuffs

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -14,6 +14,14 @@ public class PlayerControls : MonoBehaviour
 
     private Vector2 faceDirection;
 
+    [SerializeField] bool weaponEquipped, isP1;
+    [SerializeField] weapon m_weapon;
+    [SerializeField] Transform attachPoint;
+    [SerializeField] Camera m_camera;
+
+    public bool IsP1() { return isP1; }
+    public Transform GetAttachPoint() { return attachPoint; }
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -29,10 +37,31 @@ public class PlayerControls : MonoBehaviour
         Move();
         Turn();
     }
-
     public void OnMove(InputAction.CallbackContext context)
     {
         moveDirection = context.ReadValue<Vector2>();
+    }
+
+    bool modifierPressed;
+    bool buttonDown = false;
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        modifierPressed = context.performed;
+        if (modifierPressed)
+        {
+            if (!buttonDown)
+            {
+                buttonDown = true;
+
+                if (weaponEquipped)
+                {
+                    weaponEquipped = !m_weapon.Throw();
+                }
+            }
+        } else
+        {
+            buttonDown = false;
+        }
     }
 
     public void OnFaceDirection(InputAction.CallbackContext context)
@@ -57,5 +86,14 @@ public class PlayerControls : MonoBehaviour
     void Turn()
     {
         // this.transform.LookAt(faceDirection);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.tag == "weapon")
+        {
+            weaponEquipped = m_weapon.PickUp(this);
+        }
     }
 }
