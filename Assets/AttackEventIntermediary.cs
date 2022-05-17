c41da062a1b72ff889fa616d0c750f75faee87ac
Amy He <amyh11325@gmail.com>
2022-05-16 19:54:31 -0700

    added hamburger attack animations, fixed sausage attack timing, modified hamburger walking animation

---
+++ Assets/AttackEventIntermediary.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackEventIntermediary : MonoBehaviour
+{
+    // An intermediary function so that Animation Events can call castAttack in enemy parent object
+    public void castAttack() {
+        this.transform.parent.gameObject.GetComponent<meleeEnemy>().castAttack(100, 200, 10);
+    }
+}
--- Assets/EnemyStuff/enemyScripts/dashingEnemy.cs
+++ Assets/EnemyStuff/enemyScripts/dashingEnemy.cs
@@ -6,7 +6,10 @@ public class dashingEnemy : meleeEnemy
 {
     float startTime;    // Track time for step cycle
     [SerializeField] protected AnimationCurve stepCurve;    // Custom curve to control forward steps
+    [SerializeField] protected AnimationCurve offsetCurve;    // Custom curve to control upward offset during steps
+    [SerializeField] protected float step_period_time = 2.0f;
     bool abilityStep = false;   // True if current step uses ability
+    bool animationTriggered = false; // Used to ensure animation only triggers once
 
     private void Start()
     {
@@ -37,7 +40,7 @@ public class dashingEnemy : meleeEnemy
     }
 
     protected override void moveForward(float spd) {
-        float t = (Time.time - startTime)/2.0f;
+        float t = (Time.time - startTime)/step_period_time;
         if (t >= 1) {
             startTime = Time.time; 
             t = 0;
@@ -46,14 +49,19 @@ public class dashingEnemy : meleeEnemy
             if (abilityCast > 0) {
                 abilityStep = true;
                 abilityCast--;
+                animationTriggered = false;
             } else {
                 abilityStep = false;
-                attackObj.SetActive(false);
             }
         }
         if (abilityStep) {
+            if (stepCurve.Evaluate(t) > 0 && !animationTriggered) {
+                // trigger animation when hamburger starts dashing forward
+                animator.SetTrigger("isAttacking");
+                animationTriggered = true; 
+            }
             spd = spd * 5.0f;
         }
-        trfm.position += trfm.up * stepCurve.Evaluate(t) * spd;
+        trfm.position += trfm.up * stepCurve.Evaluate(t) * spd + (new Vector3(0, offsetCurve.Evaluate(t) * .01f, 0));
     }
 }
